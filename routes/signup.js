64a4843235ffafa1797a2a1806b56615629410fd
ikenna <enebret@yahoo.com>
2022-03-25 14:24:40 +0100

    edits

--- routes/signup.js
+++ routes/signup.js
@@ -11,7 +11,10 @@ const tokenSecret = 'test-app';
 router.get('/signup', (req,res) => res.send('User'));
 
 router.post('/signup', (req, res) => {
-    console.log(req.body);
+    console.log(req.body.email);
+    //check if user already has an account else proceed to creating a new account
+    //hash password
+    //redirect to dashboard
     const newUser = new User(req.body); //using model
             newUser.save()
             .then(user => {
@@ -22,6 +25,19 @@ router.post('/signup', (req, res) => {
             });
 });
 
-
+router.get('/login', (req,res) => {
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if(!user){
+            res.status(404).json({error: 'no user with email found'});
+        }
+        else if(req.body.password===user.password  ){
+            //validate password
+            //set cookie
+            //redirect to dahsboard
+            res.send('welcome');
+        }
+    });
+});
 
 module.exports = router;
