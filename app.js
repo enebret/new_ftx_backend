0e5632c428e8e8cf43a70c140d852ccee03a3b3a
ene ikenna <enebret@yahoo.com>
2022-03-27 07:12:22 +0100

    relevant additions to the stack to activate backend connection

--- app.js
+++ app.js
@@ -4,15 +4,36 @@ const express = require('express');
 var bodyParser = require('body-parser');
 const userR = require('./routes/signup.js');
 const connectDB = require('./config/db');
+const User = require('./models/user');
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 connectDB();
 
 
+app.use('/login', function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+app.post('/login', (req, res) => {
+    const newUser = new User(req.body); //using model
+    newUser.save()
+    .then(user => {
+        //send response to react frontend
+        console.log(user);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
 
 app.use('/user', userR);
 app.use(express.json());
